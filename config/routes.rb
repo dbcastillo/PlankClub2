7850d5183fe1085642a5a540a20ab2356edcab87
ma539 <ma539@cornell.edu>
2018-08-23 11:04:21 -0400

    Added styling with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :students
   resources :planks
+  resources :welcome, only: [:index]
   get '/students/:id/planks/new', to: "student_planks#new", as: 'new_student_plank'
   post '/student/:id/planks', to: "student_planks#create", as: 'student_planks'
   get '/student/:id/planks/:sp_id/edit', to: 'student_planks#edit', as: 'edited_plank'
@@ -8,4 +9,5 @@ Rails.application.routes.draw do
   delete '/student/:id/planks/:sp_id/delete', to: 'student_planks#destroy', as: 'deleted_plank'
   get '/student/:id/planks/:sp_id', to: 'student_plank#show'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
